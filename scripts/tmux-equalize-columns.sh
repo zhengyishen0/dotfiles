8ea733da43b8800ef4dd927080f6378ca7fa2fb0
Zhengyi Shen <zhengyishen1@gmail.com>
2026-01-25 18:42:10 +0800

    tmux: fix column equalize to work with 4+ columns

--- scripts/tmux-equalize-columns.sh
+++ scripts/tmux-equalize-columns.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-# Equalize column widths without affecting vertical stacks
+# Equalize column widths
 
-window_width=$(tmux display-message -p '#{window_width}')
+equalize() {
+    local window_width=$(tmux display-message -p '#{window_width}')
+    local cols=($(tmux list-panes -F '#{pane_left}' | sort -nu))
+    local num_cols=${#cols[@]}
+    
+    [ "$num_cols" -le 1 ] && return
+    
+    local usable=$((window_width - num_cols + 1))
+    local target=$((usable / num_cols))
+    
+    # Resize each column except last, re-reading positions each time
+    for ((i = 0; i < num_cols - 1; i++)); do
+        cols=($(tmux list-panes -F '#{pane_left}' | sort -nu))
+        local col_left=${cols[$i]}
+        local pane=$(tmux list-panes -F '#{pane_id} #{pane_left}' | awk -v l="$col_left" '$2 == l {print $1; exit}')
+        [ -n "$pane" ] && tmux resize-pane -t "$pane" -x "$target" 2>/dev/null
+    done
+}
 
-# Get unique column left positions (sorted numerically)
-cols=($(tmux list-panes -F '#{pane_left}' | sort -nu))
-num_cols=${#cols[@]}
-
-[ "$num_cols" -le 1 ] && exit 0
-
-# Calculate target width (window_width - borders) / num_cols
-# Borders: 1 char between each column = num_cols - 1
-usable=$((window_width - num_cols + 1))
-target=$((usable / num_cols))
-
-# Resize columns left to right, except last (gets remaining space)
-for ((i = 0; i < num_cols - 1; i++)); do
-    col_left=${cols[$i]}
-    # Get first pane at this column position
-    pane=$(tmux list-panes -F '#{pane_id} #{pane_left}' | awk -v l="$col_left" '$2 == l {print $1; exit}')
-    [ -n "$pane" ] && tmux resize-pane -t "$pane" -x "$target" 2>/dev/null
-done
+# Run multiple times to converge
+equalize
+equalize
+equalize
