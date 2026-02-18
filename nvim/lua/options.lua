9af9fbeeabdf3e0673540eb6a47229979c1cdc96
Zhengyi Shen <zhengyishen1@gmail.com>
2026-02-18 21:26:01 +0800

    nvim: disable equalalways to preserve split sizes

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,3 +6,4 @@ local o = vim.o
 o.cmdheight = 0  -- Remove gap at bottom (command line only shows when needed)
 o.relativenumber = true  -- Show relative line numbers for easy {n}j/k navigation
 o.selectmode = "mouse"   -- Mouse selection uses select mode (typing replaces)
+o.equalalways = false    -- Don't auto-resize windows when opening/closing splits
